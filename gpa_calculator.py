d2e47dedd0125e95604e9d594f26fa3eddc82d6b
Lukas Galic <galic.lukas@hotmail.com>
2024-12-16 22:11:40 +0100

    Added functionality for multipage transcripts

--- gpa_calculator.py
+++ gpa_calculator.py
@@ -4,19 +4,17 @@ import re
 import os
 
 # Regexes
-code_reg = r'([A-Z]{4}\d{2})'
 scope_reg = r'\b(\d+\.\d+)\s*hp\b'
 grade_reg = r'(?<!\S)([G345])(?!\S)'
 
 
 @dataclass
 class Course:
-    code: str
     scope: float
     grade: int
 
 
-pdf_file = "Records.pdf"
+pdf_file = "records.pdf"
 reader = PdfReader(pdf_file)
 
 courses = []
@@ -26,30 +24,25 @@ for page in reader.pages:
     # Split the lines at the last integer on each line
     # and not on actual newlines in the pdf
     lines = re.split(r'\b(?<!\S)(1|2)(?!\S)\b', text)
-
     for line in lines:
-        code_match = re.search(code_reg, line)
-
         scope_match = re.search(scope_reg, line)
 
         grade_match = re.search(grade_reg, line)
 
-        if code_match and scope_match and grade_match:
-            course_code = code_match.group()
+        if scope_match and grade_match:
             course_scope = scope_match.group().replace("hp", "")
             course_grade = grade_match.group()
             courses.append(Course(
-                code=course_code,
                 scope=float(course_scope),
                 grade=int(course_grade) if course_grade.isdigit(
                 ) else course_grade
             ))
 
-# Extract text from page 2
+
 text = reader.pages[1].extract_text()
 
 # Total HP
-match = re.search(r'\b(\d+\.\d+)\s*hp\b', text)
+match = re.search(r'\n(\d+\.\d+)\shp\n', text)
 
 total_hp = float(match.group().replace("hp", ""))
 
